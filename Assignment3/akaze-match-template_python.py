57e9ad05bce3a54c97ecd0b0b9bcbb6fbdaacee2
Luke Harper <lukeharper@live.ca>
2017-03-30 13:33:25 -0400

    a3 q2 complete

--- Assignment3/akaze-match-template_python.py
+++ Assignment3/akaze-match-template_python.py
@@ -18,15 +18,29 @@ matches = bf.match(des1,des2)
 
 # Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
-#print matches
 
-src_pts = np.float32([ kp1[matches[m].queryIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
-dst_pts = np.float32([ kp2[matches[m].trainIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
-#print src_pts
-#print dst_pts
+src_pts = np.float32([kp1[matches[m].queryIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
+dst_pts = np.float32([kp2[matches[m].trainIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
 
-cv2.imshow('half', img1)
-cv2.imshow('long', img2)
+
+
+##################################################################################
+# Added Code
+#################################################################################
+# Calculate the homography between the src and dst points
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
+
+# numpy array has their dimensions backwards
+height, width = img2.shape
+
+# use the homography to warp image 2 onto image1
+warp_img = cv2.warpPerspective(img1, M, (width, height))
+pano_img = cv2.bitwise_or(warp_img, img2)
+
+cv2.imshow('Warp', warp_img)
+cv2.imwrite('q2-warp.jpg', warp_img)
+cv2.imshow('Panorama', pano_img)
+cv2.imwrite('q2-panorama.jpg', warp_img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
